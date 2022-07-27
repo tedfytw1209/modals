6e438779313fedb3d86ea081f1240674f90e8185
teddy <teddytw1209@gmail.com>
2022-07-27 11:25:04 +0800

    fix

--- modals/operation_tseries.py
+++ modals/operation_tseries.py
@@ -286,274 +286,9 @@ def freq_shift(X, max_shift, sfreq=100, random_state=None, *args, **kwargs):
         f_shift=delta_freq,
     )
     return transformed_X
-
-
-def _torch_normalize_vectors(rr):
-    """Normalize surface vertices."""
-    new_rr = rr.clone()
-    size = torch.linalg.norm(rr, axis=1)
-    mask = (size > 0)
-    if not torch.all(mask):
-        new_rr[mask] = rr[mask] / size[mask].unsqueeze(-1)
-    else:
-        # For some reason, this distinction is necessary when the input (rr)
-        # depends on learnable parameters. Passing a mask made of all True
-        # will lead to a RuntimeError: number of dims don't match in permute
-        new_rr = rr / size.unsqueeze(-1)
-    return new_rr
-
-def _torch_legval(x, c, tensor=True):
-    """
-    Evaluate a Legendre series at points x.
-    If `c` is of length `n + 1`, this function returns the value:
-    .. math:: p(x) = c_0 * L_0(x) + c_1 * L_1(x) + ... + c_n * L_n(x)
-    The parameter `x` is converted to an array only if it is a tuple or a
-    list, otherwise it is treated as a scalar. In either case, either `x`
-    or its elements must support multiplication and addition both with
-    themselves and with the elements of `c`.
-    If `c` is a 1-D array, then `p(x)` will have the same shape as `x`.  If
-    `c` is multidimensional, then the shape of the result depends on the
-    value of `tensor`. If `tensor` is true the shape will be c.shape[1:] +
-    x.shape. If `tensor` is false the shape will be c.shape[1:]. Note that
-    scalars have shape (,).
-    Trailing zeros in the coefficients will be used in the evaluation, so
-    they should be avoided if efficiency is a concern.
-    Parameters
-    ----------
-    x : array_like, compatible object
-        If `x` is a list or tuple, it is converted to an ndarray, otherwise
-        it is left unchanged and treated as a scalar. In either case, `x`
-        or its elements must support addition and multiplication with
-        with themselves and with the elements of `c`.
-    c : array_like
-        Array of coefficients ordered so that the coefficients for terms of
-        degree n are contained in c[n]. If `c` is multidimensional the
-        remaining indices enumerate multiple polynomials. In the two
-        dimensional case the coefficients may be thought of as stored in
-        the columns of `c`.
-    tensor : boolean, optional
-        If True, the shape of the coefficient array is extended with ones
-        on the right, one for each dimension of `x`. Scalars have dimension 0
-        for this action. The result is that every column of coefficients in
-        `c` is evaluated for every element of `x`. If False, `x` is broadcast
-        over the columns of `c` for the evaluation.  This keyword is useful
-        when `c` is multidimensional. The default value is True.
-        .. versionadded:: 1.7.0
-    Returns
-    -------
-    values : ndarray, algebra_like
-        The shape of the return value is described above.
-    See Also
-    --------
-    legval2d, leggrid2d, legval3d, leggrid3d
-    Notes
-    -----
-    The evaluation uses Clenshaw recursion, aka synthetic division.
-    Examples
-    --------
-    """
-    c = torch.as_tensor(c)
-    c = c.double()
-    if isinstance(x, (tuple, list)):
-        x = torch.as_tensor(x)
-    if isinstance(x, torch.Tensor) and tensor:
-        c = c.view(c.shape + (1,) * x.ndim)
-
-    c = c.to(x.device)
-
-    if len(c) == 1:
-        c0 = c[0]
-        c1 = 0
-    elif len(c) == 2:
-        c0 = c[0]
-        c1 = c[1]
-    else:
-        nd = len(c)
-        c0 = c[-2]
-        c1 = c[-1]
-        for i in range(3, len(c) + 1):
-            tmp = c0
-            nd = nd - 1
-            c0 = c[-i] - (c1 * (nd - 1)) / nd
-            c1 = tmp + (c1 * x * (2 * nd - 1)) / nd
-    return c0 + c1 * x
-
-
-def _torch_calc_g(cosang, stiffness=4, n_legendre_terms=50):
-    """Calculate spherical spline g function between points on a sphere.
-    Parameters
-    ----------
-    cosang : array-like of float, shape(n_channels, n_channels)
-        cosine of angles between pairs of points on a spherical surface. This
-        is equivalent to the dot product of unit vectors.
-    stiffness : float
-        stiffness of the spline.
-    n_legendre_terms : int
-        number of Legendre terms to evaluate.
-    Returns
-    -------
-    G : np.ndrarray of float, shape(n_channels, n_channels)
-        The G matrix.
-    """
-    factors = [(2 * n + 1) / (n ** stiffness * (n + 1) ** stiffness *
-                              4 * np.pi)
-               for n in range(1, n_legendre_terms + 1)]
-    return _torch_legval(cosang, [0] + factors)
-
-
-def _torch_make_interpolation_matrix(pos_from, pos_to, alpha=1e-5):
-    """Compute interpolation matrix based on spherical splines.
-    Implementation based on [1]
-    Parameters
-    ----------
-    pos_from : np.ndarray of float, shape(n_good_sensors, 3)
-        The positions to interpoloate from.
-    pos_to : np.ndarray of float, shape(n_bad_sensors, 3)
-        The positions to interpoloate.
-    alpha : float
-        Regularization parameter. Defaults to 1e-5.
-    Returns
-    -------
-    interpolation : np.ndarray of float, shape(len(pos_from), len(pos_to))
-        The interpolation matrix that maps good signals to the location
-        of bad signals.
-    References
-    ----------
-    [1] Perrin, F., Pernier, J., Bertrand, O. and Echallier, JF. (1989).
-        Spherical splines for scalp potential and current density mapping.
-        Electroencephalography Clinical Neurophysiology, Feb; 72(2):184-7.
-    """
-    pos_from = pos_from.clone()
-    pos_to = pos_to.clone()
-    n_from = pos_from.shape[0]
-    n_to = pos_to.shape[0]
-
-    # normalize sensor positions to sphere
-    pos_from = _torch_normalize_vectors(pos_from)
-    pos_to = _torch_normalize_vectors(pos_to)
-
-    # cosine angles between source positions
-    cosang_from = torch.matmul(pos_from, pos_from.T)
-    cosang_to_from = torch.matmul(pos_to, pos_from.T)
-    G_from = _torch_calc_g(cosang_from)
-    G_to_from = _torch_calc_g(cosang_to_from)
-    assert G_from.shape == (n_from, n_from)
-    assert G_to_from.shape == (n_to, n_from)
-
-    if alpha is not None:
-        G_from.flatten()[::len(G_from) + 1] += alpha
-
-    device = G_from.device
-    C = torch.vstack([
-        torch.hstack([G_from, torch.ones((n_from, 1), device=device)]),
-        torch.hstack([
-            torch.ones((1, n_from), device=device),
-            torch.as_tensor([[0]], device=device)])
-    ])
-    C_inv = torch.linalg.pinv(C)
-
-    interpolation = torch.hstack([
-        G_to_from,
-        torch.ones((n_to, 1), device=device)
-    ]).matmul(C_inv[:, :-1])
-    assert interpolation.shape == (n_to, n_from)
-    return interpolation
-
-
-def _rotate_signals(X, rotations, sensors_positions_matrix, spherical=True):
-    sensors_positions_matrix = sensors_positions_matrix.to(X.device)
-    rot_sensors_matrices = [
-        rotation.matmul(sensors_positions_matrix) for rotation in rotations
-    ]
-    if spherical:
-        interpolation_matrix = torch.stack(
-            [torch.as_tensor(
-                _torch_make_interpolation_matrix(
-                    sensors_positions_matrix.T, rot_sensors_matrix.T
-                ), device=X.device
-            ).float() for rot_sensors_matrix in rot_sensors_matrices]
-        )
-        return torch.matmul(interpolation_matrix, X)
-    else:
-        transformed_X = X.clone()
-        sensors_positions = list(sensors_positions_matrix)
-        for s, rot_sensors_matrix in enumerate(rot_sensors_matrices):
-            rot_sensors_positions = list(rot_sensors_matrix.T)
-            for time in range(X.shape[-1]):
-                interpolator_t = Rbf(*sensors_positions, X[s, :, time])
-                transformed_X[s, :, time] = torch.from_numpy(
-                    interpolator_t(*rot_sensors_positions)
-                )
-        return transformed_X
-
-
-def make_rotation_matrix(axis, angle, degrees=True):
-    assert axis in ['x', 'y', 'z'], "axis should be either x, y or z."
-
-    if isinstance(angle, (float, int, np.ndarray, list)):
-        angle = torch.as_tensor(angle)
-
-    if degrees:
-        angle = angle * np.pi / 180
-
-    device = angle.device
-    zero = torch.zeros(1, device=device)
-    rot = torch.stack([
-        torch.as_tensor([1, 0, 0], device=device),
-        torch.hstack([zero, torch.cos(angle), -torch.sin(angle)]),
-        torch.hstack([zero, torch.sin(angle), torch.cos(angle)]),
-    ])
-    if axis == "x":
-        return rot
-    elif axis == "y":
-        rot = rot[[2, 0, 1], :]
-        return rot[:, [2, 0, 1]]
-    else:
-        rot = rot[[1, 2, 0], :]
-        return rot[:, [1, 2, 0]]
-
-def random_rotation(X, axis, max_degrees, sensors_positions_matrix,
-                    spherical_splines, random_state=None, *args, **kwargs):
-    rng = check_random_state(random_state)
-    random_angles = torch.as_tensor(rng.uniform(
-        low=0,
-        high=1,
-        size=X.shape[0]
-    ), device=X.device) * 2 * max_degrees - max_degrees
-    rots = [
-        make_rotation_matrix(axis, random_angle, degrees=True)
-        for random_angle in random_angles
-    ]
-    rotated_X = _rotate_signals(
-        X, rots, sensors_positions_matrix, spherical_splines
-    )
-    return rotated_X
-
-def get_standard_10_20_positions(raw_or_epoch=None, ordered_ch_names=None):
-    """Returns standard 10-20 sensors position matrix (for instantiating
-    RandomSensorsRotation for example).
-
-    Parameters
-    ----------
-    raw_or_epoch : mne.io.Raw | mne.Epoch, optional
-        Example of raw or epoch to retrive ordered channels list from. Need to
-        be named as in 10-20. By default None.
-    ordered_ch_names : list, optional
-        List of strings representing the channels of the montage considered.
-        The order has to be consistent with the order of channels in the input
-        matrices that will be fed to `RandomSensorsRotation` transform. By
-        default None.
-    """
-    assert raw_or_epoch is not None or ordered_ch_names is not None,\
-        "At least one of raw_or_epoch and ordered_ch_names is needed."
-    if ordered_ch_names is None:
-        ordered_ch_names = raw_or_epoch.info['ch_names']
-    ten_twenty_montage = make_standard_montage('standard_1020')
-    positions_dict = ten_twenty_montage.get_positions()['ch_pos']
-    positions_subdict = {
-        k: positions_dict[k] for k in ordered_ch_names if k in positions_dict
-    }
-    return np.stack(list(positions_subdict.values())).T
+'''
+Delete EEG special transfroms
+'''
 
 ### for ECG transfrom
 #tseries signal (batch,channel,len)
@@ -645,7 +380,7 @@ def Window_Slicing_Circle(X, magnitude,window_size=1000, random_state=None, *arg
     x = X.detach().cpu().numpy()
     dup_x = np.concatenate([x,x],axis=2)
     window_start = rng.randint(0, dup_x.shape[2] - window_size)
-    new_x = x[:,:,window_start:window_start+window_size]
+    new_x = dup_x[:,:,window_start:window_start+window_size]
     new_x = torch.from_numpy(new_x).float()
     return new_x
 #TS_Permutation: tseries signal (batch,channel,len)
@@ -677,13 +412,14 @@ def time_warp(x,rng, sigma=0.2, knot=4): #ref (batch, time_steps, channel)
     
     orig_steps = np.arange(x.shape[1])
     
-    random_warps = rng.normal(loc=1.0, scale=sigma, size=(x.shape[0], knot+2, x.shape[2]))
+    random_warps = rng.normal(loc=1.0, scale=sigma, size=(x.shape[0], knot+2, 1)) #modify for same warp between channels
+
     warp_steps = (np.ones((x.shape[2],1))*(np.linspace(0, x.shape[1]-1., num=knot+2))).T
     
     ret = np.zeros_like(x)
     for i, pat in enumerate(x):
         for dim in range(x.shape[2]):
-            time_warp = CubicSpline(warp_steps[:,dim], warp_steps[:,dim] * random_warps[i,:,dim])(orig_steps)
+            time_warp = CubicSpline(warp_steps[:,dim], warp_steps[:,dim] * random_warps[i,:,0])(orig_steps)
             scale = (x.shape[1]-1)/time_warp[-1]
             ret[i,:,dim] = np.interp(orig_steps, np.clip(scale*time_warp, 0, x.shape[1]-1), pat[:,dim]).T
     return ret
@@ -747,6 +483,8 @@ def window_warp(x,rng, window_ratio=0.1, scales=[0.5, 2.]): #ref (batch, time_st
     # https://halshs.archives-ouvertes.fr/halshs-01357973/document
     warp_scales = rng.choice(scales, x.shape[0])
     warp_size = np.ceil(window_ratio*x.shape[1]).astype(int)
+    if warp_size<=1:
+        return x
     window_steps = np.arange(warp_size)
         
     window_starts = rng.randint(low=1, high=x.shape[1]-warp_size-1, size=(x.shape[0])).astype(int)
@@ -818,9 +556,9 @@ TS_ADD_LIST = [
     (Window_Slicing, 0, 1),  # 0
     (Window_Slicing_Circle, 0, 1),  # 1
     (TS_Permutation, 2, 20),  # 2
-    (Time_Warp, 0, 1),  # 3
+    (Time_Warp, 0, 0.2),  # 3
     (Scaling, 0, 1),  # 4
-    (Magnitude_Warp, 0, 1),  # 5
+    (Magnitude_Warp, 0, 0.4),  # 5
     (Window_Warp, 0, 1),  # 6
 ]
 MAG_TEST_NAMES = [
@@ -880,13 +618,15 @@ class RandAugment:
         self.n = n
         self.m = m      # [0, 1]
         self.augment_list = TS_AUGMENT_LIST
+        self.augment_ids = [i for i in range(len(self.augment_list))]
         self.rng = check_random_state(rd_seed)
     def __call__(self, img):
         #print(img.shape)
         seq_len , channel = img.shape
         img = img.permute(1,0).view(1,channel,seq_len)
-        ops = self.rng.choices(self.augment_list, k=self.n)
-        for op, minval, maxval in ops:
+        op_ids = self.rng.choice(self.augment_ids, size=self.n)
+        for id in op_ids:
+            op, minval, maxval = self.augment_list[id]
             val = float(self.m) * float(maxval - minval) + minval
             #print(val)
             img = op(img, val,random_state=self.rng)
@@ -935,7 +675,7 @@ if __name__ == '__main__':
     'wisdm' : 10,
     'chapman' : 10,
     }
-    dataset = PTBXL(dataset_path='../../CWDA_research/CWDA/datasets/Datasets/ptbxl-dataset')
+    dataset = PTBXL(dataset_path='../CWDA_research/CWDA/datasets/Datasets/ptbxl-dataset')
     print(dataset[0])
     print(dataset[0][0].shape)
     sample = dataset[50]
@@ -946,16 +686,16 @@ if __name__ == '__main__':
     print(x.shape)
     x_tensor = torch.from_numpy(x).float()
     plot_line(t,x)
-    for name in TS_ADD_NAMES:
+    for name in TS_ADD_NAMES+TS_OPS_NAMES:
         print('='*10,name,'='*10)
-        x_aug = apply_augment(x_tensor,name,0.5).numpy()
+        x_aug = apply_augment(x_tensor,name,0).numpy()
         print(x_aug.shape)
         plot_line(t,x_aug)
-    randaug = RandAugment(1,0.5)
+    randaug = RandAugment(1,0,rd_seed=42)
     name = 'random_time_mask'
     for i in range(3):
         #print('='*10,name,'='*10)
-        x_aug = randaug(x_tensor,rd_seed=42).numpy()
+        x_aug = randaug(x_tensor).numpy()
         print(x_aug.shape)
         plot_line(t,x_aug)
     #ECG part
