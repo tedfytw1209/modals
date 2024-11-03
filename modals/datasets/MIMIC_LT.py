e9492b4db95c83c6f4cf7648fff96abc1faf33b6
teddy <teddytw1209@gmail.com>
2024-11-03 10:19:02 -0500

    Update MIMIC_LT.py

--- modals/datasets/MIMIC_LT.py
+++ modals/datasets/MIMIC_LT.py
@@ -28,8 +28,8 @@ class MIMICLT(Dataset):
         self.classes = list(self.annotations.columns[6:])
         self.num_class = len(self.classes)
         image_names, labels = self.annotations['path'], self.annotations.iloc[:, 6:]
-        print('Sample image name: ',self.image_names)
-        print('Sample label: ',self.labels)
+        print('Sample image name: ',image_names)
+        print('Sample label: ',labels)
         self.image_names = [image_name for image_name in image_names]
         self.labels = [label.to_numpy() for label in labels]
         
